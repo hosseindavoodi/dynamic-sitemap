7c7858ff9909e2a5b202120162ed4d3430bf16eb
Hossein Davoodi <h.davoodi@trenkwalder.com>
2022-12-25 11:49:23 +0300

    main logic of adding serverside rendered function is made

---
+++ pages/sitemap.ts
@@ -0,0 +1,34 @@
+const DataUrl = "https://jsonplaceholder.typicode.com/posts";
+
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://jsonplaceholder.typicode.com</loc>
+     </url>
+     <url>
+       <loc>https://jsonplaceholder.typicode.com/guide</loc>
+     </url>
+     
+   </urlset>
+ `;
+}
+
+function SiteMap() {}
+
+export async function getServerSideProps({ res }: any) {
+  const request = await fetch(DataUrl);
+  const posts = await request.json();
+  const sitemap = generateSiteMap();
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
